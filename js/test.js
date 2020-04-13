00754a71c28917a1baf962bb351df1f3607b1a25
Richard Alexander Corro Chirinos <rcorroch@isimtl.local>
2020-04-13 17:54:39 -0400

    creations des balises et events done

--- js/test.js
+++ js/test.js
@@ -1,65 +1,74 @@
 
 
-
-/*window.addEventListener('DOMContentLoaded', function loaded (event) {
-    window.removeEventListener('DOMContentLoaded', loaded, false)
-*/
-    // Code a exécuter
-
-
 /*const card = document.querySelector('.card');
 card.addEventListener( 'click', function() {
   card.classList.toggle('is-flipped');
 });*/
 const nbrCards=17;
 const max=8;
-let nb=1;
-const bodyElement=document.getElementsByTagName('body')
+let indexOfRepeatedCards=1;
+const divContainer=document.getElementById('cardContainer')
+
+/*
+
+VOIR FUNCTION ARRAY POUR LES NUMEROS DE LES IMAGES
+function randomNumberCalculator(){
+    const randomNumber=Math.floor(Math.random() * 8) + 1;
+    const numberArray=[randomNumber];
+    console.log(numberArray)
+    return randomNumber
+}
+*/
+
 
 for (let index = 1; index < nbrCards; index++) {
-    /*creation de la premier balise*/
-    const cardsAcreer=document.createElement('div')
-    cardsAcreer.classList.add('scene','scene--card')
-    document.body.appendChild(cardsAcreer)
-    /*creation de la deuxime balise*/
-    const divClassCard=document.createElement('div')
-    divClassCard.classList.add('card')
-    cardsAcreer.appendChild(divClassCard)
 
-    /* creation de la balise qui contien la image frontal  */
+        /*creation de la premier balise*/
+        const cardsAcreer=document.createElement('div')
+        cardsAcreer.classList.add('scene','scene--card')
+        divContainer.appendChild(cardsAcreer)
+        /*creation de la deuxime balise*/
+        const divClassCard=document.createElement('div')
+        divClassCard.classList.add('card')
+        divClassCard.addEventListener( 'click', function() {
+            divClassCard.classList.toggle('is-flipped');
+          });
+        cardsAcreer.appendChild(divClassCard)
 
-    const divFaceFront=document.createElement('div')
-    divFaceFront.classList.add('card__face','card__face--front')
-    divClassCard.appendChild(divFaceFront)
+        /* creation de la balise qui contien la image frontal  */
 
-    /*creation et inclusion de la img dans la div class face-front */
+        const divFaceFront=document.createElement('div')
+        divFaceFront.classList.add('card__face','card__face--front')
+        divClassCard.appendChild(divFaceFront)
 
-    const imgFront=document.createElement('img')
-    imgFront.src='photos/Interrogation.png'
-    imgFront.setAttribute("alt", "Interrogation");
-    divFaceFront.appendChild(imgFront)
+        /*creation et inclusion de la img dans la div class face-front */
 
-    /*Creation de la  balise qui contient la image arriere */
+        const imgFront=document.createElement('img')
+        imgFront.src='photos/Interrogation.png'
+        imgFront.setAttribute("alt", "Interrogation");
+        divFaceFront.appendChild(imgFront)
 
-    const divFaceBack=document.createElement('div')
-    divFaceBack.classList.add('card__face','card__face--back')
-    divClassCard.appendChild(divFaceBack)
+        /*Creation de la  balise qui contient la image arriere */
 
-    /*creation et inclusion de la img dans la div class face-back */
-    if (index<=8) {
-    const imgBack=document.createElement('img')
-    imgBack.src='photos/Car'+index+'.png'
-    imgBack.setAttribute("alt", "Car"+index);
-    divFaceBack.appendChild(imgBack)
+        const divFaceBack=document.createElement('div')
+        divFaceBack.classList.add('card__face','card__face--back')
+        divClassCard.appendChild(divFaceBack)
 
-    }else if (index===8) {
-    const imgBack=document.createElement('img')
-    imgBack.src='photos/Car'+nb+'.png'
-    imgBack.setAttribute("alt", "Car"+nb);
-    divFaceBack.appendChild(imgBack)
-    console.log(nb)
-    }
-    nb++
-}
+        /*creation et inclusion de la img dans la div class face-back */
+        if (index <= max) {
+        const imgBack=document.createElement('img')
+        imgBack.src='photos/Car'+index+'.png'
+        imgBack.setAttribute("alt", "Car"+index);
+        divFaceBack.appendChild(imgBack)
 
-/*}, false)*/
+        }else{
+        const imgBack=document.createElement('img')
+        imgBack.src='photos/Car'+indexOfRepeatedCards+'.png'
+        imgBack.setAttribute("alt", "Car"+indexOfRepeatedCards);
+        divFaceBack.appendChild(imgBack)
+        indexOfRepeatedCards++
+        }
+
+
+ }
+
